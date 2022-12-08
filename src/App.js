b16e30a1f9bad3da7bfef126c86360790eb6eac4
Artem Kosharnyi <artemkkosharnyi@gmali.com>
2022-12-08 17:03:57 +0200

    ReportRoute

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Main from "./Components/MAin/Main";
-
+import { Routes, Route } from "react-router-dom";
+import Reports from "./Components/Routes/Reports/Reports";
+import Needs from "./Components/Routes/Needs";
+import Team from "./Components/Routes/Team/Team";
+import Donate from "./Components/Routes/Donate";
 
 function App() {
-  return <div className=" flex flex-col  relative w-full h-full ">
-    <Header/>
-   <Main/>
-  </div>;
+  return (
+    <div className=" flex flex-col  relative w-full h-full ">
+      <Header  />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/Reports" element={<Reports/>}/>
+        <Route path="/Needs" element={<Needs/>}/>
+        <Route path="/Team" element={<Team/>}/>
+        <Route path="/Donate" element={<Donate/>}/>
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
-
 import NavLinkOrg from "./NavLinkORG/NavLinkOrg";
 import NavList from "./NavList/NavList";
 
 export default function Header() {
   return (
-    <header className=" flex justify-between fixed z-10 top-0 right-0  w-full h-auto bg-[#010321] text-white py-4 px-8 text-lg">
-     <NavList/>
-     <NavLinkOrg/>
+    <header>
+      <nav className=" flex justify-between fixed z-10 top-0 right-0  w-full h-auto bg-[#010321] text-white py-4 px-8 text-lg">
+        <NavList />
+        <NavLinkOrg />
+      </nav>
     </header>
   );
 }
